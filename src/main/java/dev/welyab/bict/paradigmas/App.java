23b2717cbde9528b5d37ed3be60d9fb7087f92b6
Welyab Paula <welyab.exe@gmail.com>
2021-12-10 11:39:15 -0300

    initil commit

---
+++ src/main/java/dev/welyab/bict/paradigmas/App.java
@@ -0,0 +1,13 @@
+package dev.welyab.bict.paradigmas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
